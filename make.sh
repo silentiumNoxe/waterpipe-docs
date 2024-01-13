3cfbea848820d63faff284600a26d2c1c707533a
silentiumnoxe <silentium.noxe@gmail.com>
2024-01-13 20:09:48 +0200

    update index.html

--- make.sh
+++ make.sh
@@ -1,7 +1,7 @@
 echo "Getting started"
 
 # Bundle docs into zero-dependency HTML file
-npx redoc-cli bundle openapi/openapi.yaml && \
+npx @redocly/cli build-docs ./openapi.yaml && \
 mv redoc-static.html index.html && \
 echo "Changed name from redoc-static.html to index.html" && \
 # Add favicon
